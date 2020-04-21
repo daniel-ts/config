47f4f51350e770c39d051c5d6c2aba4f64bfc8ee
Daniel Tschertkow <daniel.tschertkow@posteo.de>
2020-04-21 10:35:23 +0200

    added offline imap and emacs-pdf.desktop

---
+++ .config/offlineimap/offlineimap.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+
+from subprocess import check_output
+import json
+
+user = "daniel.tschertkow@posteo.de"
+path = "~/.config/offlineimap/passwords.json.gpg"
+
+
+def decrypt_store():
+    return check_output(
+        "gpg --quiet --decrypt --batch --recipient {} {}".format(user, path),
+        shell=True
+    ).strip("\n")
+
+def get_username(acc):
+    return json.loads( decrypt_path() )[acc]['user']
+
+def get_password(acc):
+    return json.loads( decrypt_path() )[acc]['pass']
